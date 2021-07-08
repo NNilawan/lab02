61aba2f3183421b4d58082d84afb27aced5ce6e4
NNilawan <nilawan_l@cmu.ac.th>
2021-07-08 23:55:01 +0700

    Q5 5.2

--- main.js
+++ main.js
@@ -6,5 +6,11 @@ const app = Vue.createApp({
             // 4.9
             productDetail: 'A sock is a piece of clothing worn on the feet and often covering the ankle or some part of the calf'
         }
+    },
+    methods: {
+        updateCart() {
+            this.cart += 1;
+        }
     }
+
 })
