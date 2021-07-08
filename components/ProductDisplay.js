831432b39443169e1f0e68815f19b43f0f09d905
NNilawan <nilawan_l@cmu.ac.th>
2021-07-08 23:58:07 +0700

    Q5 5.3

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -66,7 +66,7 @@ app.component('product-display', {
     },
     methods: {
         addToCart() {
-            this.$emit('add-to-cart')
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
         },
         updateImage(variantImage) {
             this.image = variantImage
